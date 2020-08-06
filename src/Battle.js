dc4e843d924058006b8dc67a83b1ba0f76380d85
Gilad <gilad@bennun.net>
2020-08-06 17:36:01 +0300

    EITAN HW

--- src/Battle.js
+++ src/Battle.js
@@ -1,7 +1,7 @@
 export default class Battle {
-    constructor(enemyLvl, turn) {
-        this.enemyLvl = enemyLvl;
-        this.turn = turn;
+    constructor(currentTurn) {
+        this.enemyLvl = Math.floor(Math.random() * (5 - 1) + 1)
+        this.turn = Math.floor(currentTurn + (Math.random() * (3 - 1) + 1))
         this.brandChange = undefined;
         this.didWin = undefined;
     }
--- src/main-page.js
+++ src/main-page.js
@@ -3,12 +3,16 @@ import Button from '@material-ui/core/Button';
 import InitialConditions from "./Helpers/app-storage"
 import Train from "./train"
 import Gladiator from './gladiator';
+import Battle from './Battle';
 
 
 export default function MainPage() {
     const [gladiator, setGladiator] = useState(new Gladiator())
     const [gameHistory, setGameHistory] = useState({ wins: 0, losses: 0 })
     const [brand, setBrand] = useState()
+    const [turnCount, setTurnCount] = useState(1)
+    const [inBattle, setInBattle] = useState(false)
+    const [currentBattle, setCurrentBattle] = useState(new Battle(1))
     useEffect(() => {
         const bubu = InitialConditions.initializeConditions()
         setGladiator(bubu.gladiator)
@@ -17,28 +21,38 @@ export default function MainPage() {
     }, [])
     const train = attribute => {
 
-        console.log(20, {...gladiator, [attribute]: gladiator[attribute]++})
-        setGladiator({...gladiator, [attribute]: gladiator[attribute]++})
+        console.log(20, { ...gladiator, [attribute]: gladiator[attribute]++ }) //// WHY DOES THIS WORK????
+        setGladiator({ ...gladiator, [attribute]: gladiator[attribute]++ })
+        setInBattle(!inBattle)
     }
     const divstyleleft = { width: "30%", display: "grid", padding: "35px" }
     const divstyleRight = { width: "30%", display: "grid", padding: "35px" }
     return (
         <div>
+            <header style={{ textAlign: "center" }}>{gladiator?.name ?? null}</header>
             <div style={{ display: "flex" }}>
-                <header style={{ textAlign: "center" }}>{gladiator?.name ?? null}</header>
                 <div style={divstyleleft}>
                     <span style={{ textAlign: "center" }}>Brand: {brand}</span>
                     <span style={{ textAlign: "center" }}>Wins: {gameHistory.wins}</span>
                     <span style={{ textAlign: "center" }}>Losses: {gameHistory.losses}</span>
                 </div>
+                <div style={divstyleleft}>
+                    <span style={{ textAlign: "center" }}>Showmanship: {gladiator.showmanship}</span>
+                    <span style={{ textAlign: "center" }}>Martial: {gladiator.martial}</span>
+                </div>
+                <h3>Next Battle: </h3>
+                <span>Enemy Level: {currentBattle.enemyLvl}</span>
+                <span>Happens in: {currentBattle.turn-turnCount} Turns</span>
                 <div style={divstyleRight}>
                     <Button variant="contained" color="secondary" padding="35px">End Turn</Button>
                 </div>
             </div>
-            <Train
-                gladiator={gladiator}
-                train={train}
-            />
+
+            {inBattle ?
+                <h1>BATTLE</h1>:<Train
+                    gladiator={gladiator}
+                    train={train}
+                />}
         </div>
     );
 }
